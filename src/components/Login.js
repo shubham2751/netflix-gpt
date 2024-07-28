e1a2dd74312b9747d9292b94a8fa914c3184f578
Shubham Mishra <shubhammishra2751@gmail.com>
2024-07-28 15:43:18 +0530

    Revert "Sign Up Form Created"
    This reverts commit 8406cf72db7a2bfe6a12c59d0f10ddaba578948a.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./Header";
 
 const Login = () => {
-  const [isSignInForm, setIsSignInForm] = useState(true);
-
-  const toggleSignInForm = () => {
-    setIsSignInForm(!isSignInForm);
-  };
-
   return (
     <div>
       <Header />
@@ -18,16 +12,7 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
-        <h1 className="font-bold text-3xl py-4">
-          {isSignInForm ? "Sign In" : "Sign Up"}
-        </h1>
-        {!isSignInForm && (
-          <input
-            type="text"
-            placeholder="Email Address"
-            className="p-4 my-4 w-full bg-gray-700"
-          />
-        )}
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
         <input
           type="text"
           placeholder="Email Address"
@@ -38,14 +23,10 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 my-6 w-full rounded-lg bg-red-500">
-          {isSignInForm ? "Sign In" : "Sign Up"}
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
         </button>
-        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {isSignInForm
-            ? "New to Netflix-GPT? Sign Up Now"
-            : "Already Regsitered? Sign In Now"}
-        </p>
+        <p className="py-4">New to Netflix-GPT? Sign Up Now</p>
       </form>
     </div>
   );
