4e990feaeeb96503af1389d1a3bc923aa41b4fa6
Shubham Mishra <shubhammishra2751@gmail.com>
2024-07-28 13:38:41 +0530

    Sign In Form Created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/21a8ba09-4a61-44f8-8e2e-70e949c00c6f/6678e2ea-85e8-4db2-b440-c36547313109/IN-en-20240722-POP_SIGNUP_TWO_WEEKS-perspective_WEB_3457a8b1-284d-4bb5-979e-2a2e9bb342b3_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+        <p className="py-4">New to Netflix-GPT? Sign Up Now</p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
